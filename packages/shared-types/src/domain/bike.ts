795fbdaa46435d3c6e3d7e5fbc4fadf007ffa21e
sakho13 <wecl.khotaro@gmail.com>
2025-12-05 06:52:39 +0900

    🎨 型の整理

--- packages/shared-types/src/domain/bike.ts
+++ packages/shared-types/src/domain/bike.ts
@@ -1,7 +1,47 @@
 export type BikeId = string & { readonly __brand: unique symbol }
 export const createBikeId = (id: string): BikeId => id as BikeId
 
-export interface Bike {
+export type Bike = {
   id: BikeId
   name: string
 }
+
+export type UserBikeId = string & { readonly __brand: unique symbol }
+export const createUserBikeId = (id: string): UserBikeId => id as UserBikeId
+
+export type UserBike = {
+  bikeId: BikeId
+  userBikeId: UserBikeId
+
+  serialNumber: string
+}
+
+export type MyUserBikeId = string & { readonly __brand: unique symbol }
+export const createMyUserBikeId = (id: string): MyUserBikeId =>
+  id as MyUserBikeId
+
+export type MyUserBike = {
+  bikeId: BikeId
+  userBikeId: UserBikeId
+  myUserBikeId: MyUserBikeId
+
+  nickname: string | null
+  purchaseDate: Date | null
+  purchasePrice: number | null
+  purchaseMileage: number | null
+  totalMileage: number
+
+  ownedAt: Date
+  soldAt: Date | null
+  ownStatus: UserMyBikeOwnStatus
+}
+
+export type UserMyBikeOwnStatus =
+  // 所有中
+  | 'OWN'
+  // 売却済み
+  | 'SOLD'
+  // 譲渡済み
+  | 'TRANSFERRED'
+  // 廃車
+  | 'SCRAPPED'
