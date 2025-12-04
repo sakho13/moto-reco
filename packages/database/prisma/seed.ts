ef5c26fb954f90f116e32ee6a4f56482c34bebc6
sakho13 <wecl.khotaro@gmail.com>
2025-12-04 22:06:25 +0900

    🌱 メーカーマスタのシードデータを外部ファイルに移動し、バイク情報を追加

--- packages/database/prisma/seed.ts
+++ packages/database/prisma/seed.ts
@@ -1,80 +1,5 @@
 import { prisma } from '../src/index'
-
-/**
- * メーカーマスタのシードデータ
- */
-const manufacturers = [
-  {
-    name: 'ホンダ',
-    nameEn: 'Honda',
-    websiteUrl: 'https://www.honda.co.jp/motor/',
-    country: '日本',
-    isActive: true,
-  },
-  {
-    name: 'ヤマハ',
-    nameEn: 'Yamaha',
-    websiteUrl: 'https://www.yamaha-motor.co.jp/mc/',
-    country: '日本',
-    isActive: true,
-  },
-  {
-    name: 'スズキ',
-    nameEn: 'Suzuki',
-    websiteUrl: 'https://www1.suzuki.co.jp/motor/',
-    country: '日本',
-    isActive: true,
-  },
-  {
-    name: 'カワサキ',
-    nameEn: 'Kawasaki',
-    websiteUrl: 'https://www.kawasaki-motors.com/mc/',
-    country: '日本',
-    isActive: true,
-  },
-  {
-    name: 'ドゥカティ',
-    nameEn: 'Ducati',
-    websiteUrl: 'https://www.ducati.com/',
-    country: 'イタリア',
-    isActive: true,
-  },
-  {
-    name: 'BMW',
-    nameEn: 'BMW Motorrad',
-    websiteUrl: 'https://www.bmw-motorrad.jp/',
-    country: 'ドイツ',
-    isActive: true,
-  },
-  {
-    name: 'ハーレーダビッドソン',
-    nameEn: 'Harley-Davidson',
-    websiteUrl: 'https://www.harley-davidson.com/',
-    country: 'アメリカ',
-    isActive: true,
-  },
-  {
-    name: 'トライアンフ',
-    nameEn: 'Triumph',
-    websiteUrl: 'https://www.triumph-motorcycles.jp/',
-    country: 'イギリス',
-    isActive: true,
-  },
-  {
-    name: 'KTM',
-    nameEn: 'KTM',
-    websiteUrl: 'https://www.ktm.com/',
-    country: 'オーストリア',
-    isActive: true,
-  },
-  {
-    name: 'アプリリア',
-    nameEn: 'Aprilia',
-    websiteUrl: 'https://www.aprilia.com/',
-    country: 'イタリア',
-    isActive: true,
-  },
-]
+import { manufacturers } from './seedData'
 
 async function main() {
   console.log('Start seeding...')
@@ -84,8 +9,32 @@ async function main() {
   for (const manufacturer of manufacturers) {
     const result = await prisma.mManufacturer.upsert({
       where: { name: manufacturer.name },
-      update: manufacturer,
-      create: manufacturer,
+      update: {
+        name: manufacturer.name,
+        nameEn: manufacturer.nameEn,
+        websiteUrl: manufacturer.websiteUrl,
+        country: manufacturer.country,
+        isActive: manufacturer.isActive,
+      },
+      create: {
+        name: manufacturer.name,
+        nameEn: manufacturer.nameEn,
+        websiteUrl: manufacturer.websiteUrl,
+        country: manufacturer.country,
+        isActive: manufacturer.isActive,
+
+        bikes: {
+          create:
+            manufacturer.bikes?.map((bike) => ({
+              modelName: bike.modelName,
+              displacement: bike.displacement,
+              modelYear: bike.modelYear,
+              modelCode: bike.modelCode,
+              releaseYear: bike.releaseYear,
+              releaseMonth: bike.releaseMonth,
+            })) || [],
+        },
+      },
     })
     console.log(
       `Created/Updated manufacturer: ${result.name} (${result.nameEn})`
