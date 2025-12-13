2bb44b072bfd3bb240501ee525dc5605763ae73a
sakho13 <wecl.khotaro@gmail.com>
2025-12-13 14:22:16 +0900

    🐛 docsリダイレクト処理を修正

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,36 +1,5 @@
-import Image, { type ImageProps } from 'next/image'
-import { Button } from '@repo/ui/button'
-import styles from './page.module.css'
-import Link from 'next/link'
-
-type Props = Omit<ImageProps, 'src'> & {
-  srcLight: string
-  srcDark: string
-}
-
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  )
-}
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Motorepo Docs!</h1>
-
-        <div>
-          <Link href="/ref">API Reference</Link>
-        </div>
-      </main>
-
-      <footer className={styles.footer}></footer>
-    </div>
-  )
+  return redirect('/ref')
 }
