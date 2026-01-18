35af69cff16646a72ff71a71c0ae0f46c10af9b5
sakho13 <wecl.khotaro@gmail.com>
2026-01-18 16:59:00 +0900

    🎨 lint適用

--- apps/web/app/(public)/bikes/page.tsx
+++ apps/web/app/(public)/bikes/page.tsx
@@ -1,5 +1,5 @@
-import styles from './page.module.css'
 import { prisma } from '@repo/database'
+import styles from './page.module.css'
 import { PrismaMyUserBikeRepository } from '@/lib/api/server/repositories/PrismaMyUserBikeRepository'
 
 export const metadata = {
